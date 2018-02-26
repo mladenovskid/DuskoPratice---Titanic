e1f76cd99d9695191fd459739ca2f34317c2908e
dusko mladenovski <duskomladenovski@gmail.com>
2018-02-25 19:28:22 -0500

    Age Guess
    Now that almost all attributes have been populated, Age remains last to be filled before guessing survival

--- Titanic.R
+++ Titanic.R
@@ -1,5 +1,14 @@
+#load Packages
+library(ggplot2)
+library(dplyr)
+
 #Set Directory
-  setwd('C://Users//dmladenovski//Documents//R//DuskoPratice - Titanic')
+#Laptop 
+# setwd('C://Users//dmladenovski//Documents//R//DuskoPratice - Titanic')
+       
+#Home Pc
+ setwd('C://Users//Dusko (Guest)//Documents//GitHub//DuskoPratice---Titanic')
+ 
   files <- list.files()
 
 #List files names
@@ -8,10 +17,15 @@
 #Read in Files
   Test <- read.csv2('test.csv', stringsAsFactors = FALSE, sep = ',')
   GenderSubmission <- read.csv2('test.csv', stringsAsFactors = FALSE, sep = ',')
-  TitanicInfo <- read.csv2('Titanic Info.txt', stringsAsFactors = FALSE, sep = ',')
+#TitanicInfo <- read.csv2('Titanic Info.txt', stringsAsFactors = FALSE, sep = ',')
   Train <- read.csv2('train.csv', stringsAsFactors = FALSE, sep = ',')
 
   Full <- bind_rows(Train, Test)
+  
+#Change classes
+  Full$Fare <- as.numeric(Full$Fare)
+  Full$Age <- as.integer(Full$Age)
+  Full$Survived <- as.factor(Full$Survived)
   
 #Add Title Column
   Full$Title <- gsub('(.*, )|(\\..*)', '', Full$Name)
@@ -27,7 +41,7 @@
   Full$Title[Full$Title %in% CoolTitles] <- 'Cool Title'
 
 #Table Titles
-  table(Full$Sex, Full$Title, exclude = "")
+  table(Full$Sex, Full$Title)
   
   
 #Split SurNames
@@ -35,17 +49,6 @@
                          function(x) strsplit(x, split = '[,.]')[[1]][1])
   
   
-#Add Family Size
-Full$FamilySize <- Full$SibSp + Full$Parch + 1
-  
-
-glimpse(Full)
-
-  
-#load Packages
-  library(ggplot2)
-  library(dplyr)
-
 #Explore Data - Survivors by Class
 
 Train %>%
@@ -69,16 +72,16 @@ head(Full$Name)
 
 #Visualize Data - Embarked
   table(Train$Embarked, Train$Survived)
-    # 2 Passengers do not have a point of Embarkment
 
-  ggplot(Train[Train$Embarked != "",], aes(x = Embarked, fill = factor(Survived)))+
+# 2 Passengers do not have a point of Embarkment
+
+  ggplot(Train, aes(x = Embarked, fill = factor(Survived)))+
     geom_histogram(bins = 10, stat = "count", position = "dodge")
   
 
-  #Review Embarked missing data --- Missing embarkment are most likely 1st class and embarked from C
-  
-  Embarked_full <- Full %>% filter(Embarked != '')
-    
+#Review Embarked missing data --- Missing embarkment are most likely 1st class and embarked from C
+Embarked_full <- Full %>% filter(Embarked != '')
+
 ggplot(Embarked_full, aes( x = Embarked, y = as.numeric(Fare), fill = factor(Pclass))) + 
   geom_boxplot()+
   geom_hline(yintercept = 80, col = 'red')+
@@ -86,25 +89,117 @@ ggplot(Embarked_full, aes( x = Embarked, y = as.numeric(Fare), fill = factor(Pcl
 
 #Manually edit Embark point for missing data
 Full$Embarked[Full$Embarked == ''] <- 'C'
+#Recheck Attribute
+table(Full$Embarked)
 
 
-Table(Full)
-
+#Add Family Size
+Full$FamilySize <- Full$SibSp + Full$Parch + 1
 #Visualize Data - Survival Based on Family Size
   ggplot(Full[1:891,], aes(x = FamilySize, fill = factor(Survived)))+
-    geom_bar(stat = "count", position = 'dodge', ) + 
+    geom_bar(stat = "count", position = 'dodge') + 
     scale_x_continuous(breaks = 1:11) +
     theme_classic()
+  
+  #Families larger than 4 people have bad chnce of survival
+
+Full$AgeBracket <- cut_width(Full$Age, width = 10)
 
 
-mean(Full$Cabin == '')
 
-Full$Age[is.na(Full$Age)]
+#Check Survived by Age
 
-sapply(Full, )
-Full$Age == ''
+  ggplot(Full[1:891,], aes(x = Age, fill = factor(Survived))) + 
+    geom_histogram(position = 'stack')+facet_grid(.~Sex)
+  
+  
+#check missing 
+  for (Var in names(Full)){
+    missing <- sum(is.na(Full[,Var]))
+    print(c(Var, missing)) 
+  }
+  
+  Full[Full$Fare == '' | is.na(Full$Fare),]
+  Full[Full$Fare < 1,]
 
-?is.na
+  
+  
+#Identifying Mothers and Children
+  Full$Mother <- "Not Mother"
+  Full$Mother[Full$Sex == 'female' & Full$Parch > 0 & Full$Title != 'Miss' & Full$Age > 18] <- "Mother"
+
+  Full$Child[Full$Age >= 18] <- "Adult"
+  Full$Child[Full$Age < 18] <- "Child"
+
+  table(Full$Mother, Full$Survived)
+  table(Full$Child, Full$Survived)
+
+  
+#Attempting to use Decision Tree
+
+library('rpart')
+#library('party')
+
+  for (i in names(Full)){
+  totals <- sum(is.na(Full$i))
+  print(totals)
+  }
+
+  #check missing data
+colSums(is.na(Full))
+colSums(Full == '')
+
+#remove missing Age
+
+FullAge <- Full[Full$Age != ''  & !is.na(Full$Age),]
+
+
+#predict Age
+AgeTest <- rpart(data = FullAge[1:523, ], 
+                 formula = Age ~ Pclass + SibSp + Parch + Fare + Title + FamilySize, 
+                 method = 'poisson')
+AgePredict <- predict(AgeTest, FullAge[524:1046, ])
+
+plotcp(AgeTest)
+summary(AgeTest)
+plot(AgeTest)
+
+
+rpart::
+
+
+
+
+
+
+table(AgePredict)
+hist(AgePredict)
+hist(FullAge$Age)
+
+
+plotcp(AgeTest)
+  plot(AgeTest)
+  text(AgeTest)
+
+summary(AgeTest)
+  
+Fit <- rpart(formula = Survived ~ Pclass + Sex  + Fare + Age + FamilySize, data = Full[1:400,], method = 'class')
+
+rpart::plotcp(Fit)
+plot(Fit, uniform = T)
+text(Fit, all = T, cex = .8)
+
+printcp(Fit)
+plotcp(Fit)
+rsq.rpart(Fit)
+summary(Fit)
+prune(Fit, Fit$cptable[which.min(Fit$cptable[,"xerror"]),"CP"])
+
+FirstPred <- predict(Fit, Full[401:891,], type = 'poisson')
+
+table(Full[1:491, 2], FirstPred)
+
+length(FirstPred)
 
 
 
